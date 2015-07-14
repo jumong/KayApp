56c40527d15ef0bf8bea1261051b6959220de735
Spharah <mashobane247@gmail.com>
2015-07-14 12:31:09 +0200

    fixing double tapping in windows phone dropdown

---
+++ www/js/select-directive.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    "use strict";
+ 
+    angular
+        .module('starter')
+        .directive('select', selectDirective);
+ 
+    selectDirective.$inject = [];
+ 
+    function selectDirective() {
+        return {
+            restrict: 'E',
+            replace: false,
+            link: function (scope, element) {
+                if (ionic.Platform && ionic.Platform.isWindowsPhone()) {
+                    element.attr('data-tap-disabled', 'true');
+                }
+            }
+        };
+    }
+})();
